d8b4f9bf9e27baa6c15cf50acd0464a5b5c08f9a
MalinaScroll <Malina.ScrollingIn@gmail.com>
2020-01-22 11:46:56 +0100

    Finished 7

--- src/com/company/assignment7.java
+++ src/com/company/assignment7.java
@@ -9,50 +9,50 @@ public class assignment7 {
 
         Scanner scanner = new Scanner(System.in);
 
-//        //assignment 7.1
-//        System.out.println("Assignment 7.1");
-//        int[] numbers = new int[100];
-//        int counter = 0;
-//
-//        System.out.print("Which method would you like to see (1 or 2) : ");
-//        int method = scanner.nextInt();
-//
-//        if (method == 1) {
-//            //method 1
-//            System.out.println("\nThis is method 1 :\n");
-//            for (int i = 0; i < numbers.length; i++) {
-//                int random = (int) (Math.random() * 6 + 1);
-//                numbers[i] = random;
-//            }
-//
-//            for (int number : numbers) {
-//                System.out.print(number + " ");
-//                counter++;
-//                if (counter == 10) {
-//                    counter = 0;
-//                    System.out.println();
-//                }
-//            }
-//            System.out.println("\n");
-//
-//        } else if (method == 2) {
-//            //method 2
-//            System.out.println("\nThis is method 2 :\n");
-//            for (int number : numbers) {
-//                int random = (int) (Math.random() * 6 + 1);
-//                number = random;
-//                System.out.print(number + " ");
-//                counter++;
-//                if (counter % 10 == 0) {
-//                    counter = 0;
-//                    System.out.println();
-//                }
-//            }
-//            System.out.println("\n");
-//        } else {
-//            System.out.println(method + " is an invalid method");
-//            System.exit(-1);
-//        }
+        //assignment 7.1
+        System.out.println("Assignment 7.1");
+        int[] numbers = new int[100];
+        int counter = 0;
+
+        System.out.print("Which method would you like to see (1 or 2) : ");
+        int method = scanner.nextInt();
+
+        if (method == 1) {
+            //method 1
+            System.out.println("\nThis is method 1, used for and foreach loop :\n");
+            for (int i = 0; i < numbers.length; i++) {
+                int random = (int) (Math.random() * 6 + 1);
+                numbers[i] = random;
+            }
+
+            for (int number : numbers) {
+                System.out.print(number + " ");
+                counter++;
+                if (counter == 10) {
+                    counter = 0;
+                    System.out.println();
+                }
+            }
+            System.out.println("\n");
+
+        } else if (method == 2) {
+            //method 2
+            System.out.println("\nThis is method 2, used only foreach loop:\n");
+            for (int number : numbers) {
+                int random = (int) (Math.random() * 6 + 1);
+                number = random;
+                System.out.print(number + " ");
+                counter++;
+                if (counter % 10 == 0) {
+                    counter = 0;
+                    System.out.println();
+                }
+            }
+            System.out.println("\n");
+        } else {
+            System.out.println(method + " is an invalid method");
+            System.exit(-1);
+        }
 
 
         //assignment 7.2
@@ -85,6 +85,38 @@ public class assignment7 {
         }
 
         System.out.println("Your array is : " + Arrays.toString(array1) + " which consists of " + input + " numbers");
-        System.out.println("Reversed it is : " + Arrays.toString(array2) + " which consists of " + input + " numbers");
+        System.out.println("Reversed it is : " + Arrays.toString(array2) + " which consists of " + input + " numbers\n");
+
+
+        //assignment 7.3
+        System.out.println("Assignment 7.3");
+
+        double[] array3 = new double[10];
+        double counter2 = 0;
+        double divided;
+        int higher = 0;
+
+        System.out.print("Insert 10 numbers ");
+
+        for (int i = 0; i < array3.length ; i++) {
+            array3[i] = scanner.nextDouble();
+        }
+
+        for (double count : array3){
+            counter2 += count;
+        }
+
+        divided = counter2 / array3.length;
+
+        for (double v : array3) {
+            if (v > divided) {
+                System.out.print(v + " ");
+                higher++;
+            }
+        }
+
+        System.out.println("\nYour average is " + divided);
+        System.out.println("there are " + higher + " numbers higher than the average");
+
     }
 }
